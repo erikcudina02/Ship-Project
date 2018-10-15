b187d20904bfeb9026f93a73f6a2457b5e03a53b
Erik Cudina <erik.cudina@elev.ga.lbs.se>
2018-10-15 10:48:08 +0200

    Finished

---
+++ Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+public Text TimerText;
+public float StartTime;
+ 
+	// Use this for initialization
+	void Start ()
+    {
+        StartTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = StartTime + Time.time;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+        TimerText.text = minutes + ":" + seconds;
+	}
+}
---
+++ Assets/examination.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class examination : MonoBehaviour {
+    public float speedForward = 15;
+    public Color Name;
+    public SpriteRenderer Rend;
+    // Use this for initialization
+
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, speedForward * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Den får skeppet att rotera åt höger när du trycker på D
+            transform.Rotate(0, 0, -360f * Time.deltaTime);
+            Rend.color = new Color(0, 0, 1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 180f * Time.deltaTime);
+            Rend.color = new Color(0, 1f, 0);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, (-speedForward + (speedForward / 2)) * Time.deltaTime, 0);
+
+        }
+        {
+
+        }
+
+
+
+	}
+}
